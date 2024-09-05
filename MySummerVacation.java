0676077e5bc37621c0e6e31c70156c42dc03ec2e
HarishKumar-Rishikaa <rharishkumar695@student.fuhsd.org>
2024-09-05 14:04:47 -0700

    Add files via upload

---
+++ MySummerVacation.java
@@ -0,0 +1,25 @@
+// Rishikaa Harish Kumar
+//08-27-2024
+// MySummerVacation.java
+/* the program will print what I did over the summer. 
+ * Working on: Using print, println and escape sequences for tab\t and
+ * new line \n.
+ */
+
+public class MySummerVacation 
+{
+	public static void main(String[] args)
+	{//Allman style means to put the brace below the header
+		System.out.println("\n\n\n"); //Always include 3 blank lines(3BLs)
+		System.out.println("This summer vacation, I did many things," +
+			"But the main 4 things are.");
+		
+		System.out.println("\tGoing to San Francisco with my " +
+			"Grandparents,");
+		System.out.print("\tAttending many Independence Day parties, ");
+		System.out.print("\bBasking in the lovely sun at Pismo Beach, ");
+		System.out.print("\tand going to a concert with my friends.");
+		System.out.println("It was a fantastic summer.");
+	}
+}
+
